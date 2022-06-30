7aed34f824171fe69ec4e9956f9222e0ef78e60a
junsub.hyun <guswnstjq10@gmail.com>
2022-06-30 12:29:59 +0900

    JPQL

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,6 +1,7 @@
 package jpql;
 
 import javax.persistence.*;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String args[]) {
@@ -14,18 +15,84 @@ public class JpaMain {
 
         try {
 
+//            Member member = new Member();
+//            member.setUsername("member1");
+//            member.setAge(10);
+//            em.persist(member);
+
+//            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
+////            Query query2 = em.createQuery("select m.username, m.age from Member m");
+            //결과가 하나일때
+//            Member result = query.getSingleResult();
+//            System.out.println("result = " + result);
+
+            //프로젝션
+
+//            em.flush();
+//            em.clear();
+
+//            List<Member> result = em.createQuery("select m from Member m", Member.class).getResultList();
+//            Member findMember = result.get(0);
+//            findMember.setAge(20);
+
+//            List<Team> result = em.createQuery("select t from Member m join m.team t", Team.class).getResultList();
+
+//            List<Team> result = em.createQuery("select t from Member m join m.team t", Team.class).getResultList();
+
+//            List<MemberDTO> resultList = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class).getResultList();
+//
+//            MemberDTO memberDTO = resultList.get(0);
+//            System.out.println("memberDTO.getUsername() = " + memberDTO.getUsername());
+//            System.out.println("memberDTO.getAge() = " + memberDTO.getAge());
+//
+            //페이징
+//            for(int i=0; i < 100; i++){
+//
+//                Member member = new Member();
+//                member.setUsername("member"+i);
+//                member.setAge(i);
+//                em.persist(member);
+//
+//            }
+//
+//            em.flush();
+//            em.clear();
+//
+//
+//            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
+//                    .setFirstResult(1)
+//                    .setMaxResults(10)
+//                    .getResultList();
+//
+//            System.out.println("result.size() = " + result.size());
+//            for (Member member1 : result) {
+//                System.out.println("member1 = " + member1);
+//            }
+
+            //조인
+
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
             Member member = new Member();
             member.setUsername("member1");
             member.setAge(10);
+            member.setTeam(team);
+
             em.persist(member);
 
-            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
-//            Query query2 = em.createQuery("select m.username, m.age from Member m");
-            
-            //결과가 하나일때
-            Member result = query.getSingleResult();
-            System.out.println("result = " + result);
-//test
+            em.flush();
+            em.clear();
+
+//            String query = "select m from Member m inner join m.team t";          //이너 조인
+//            String query = "select m from Member m left join m.team t";             //아웃터 조인
+//            String query = "select m from Member m , Team t where m.username = t.name";             //세타 조인
+
+            String query = "select m from Member m left join m.team t on t.name= 'teamA'";          //ON 절 사용
+            List<Member> result = em.createQuery(query,Member.class)
+                    .getResultList();
+
 
             tx.commit();
         }catch (Exception e){
